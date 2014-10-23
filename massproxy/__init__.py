49ec1d4cbc11376ce86c8275ae59ee48f91aef79
hanabi1224 <harlowmoo@gmail.com>
2014-10-23 00:54:30 -0700

    Initial commit

---
+++ massproxy/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import
+
+__title__ = 'massproxy'
+__version__ = '0.1.0'
+__author__ = 'Hanabi1224'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2014 Hanabi1224'
+
+
+class _ProxyProvider(object):
+    pass
